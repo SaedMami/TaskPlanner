434751d024db8d6cb4aec5c4dba072babfbe454a
Sad Mami <sad.mami@altran.com>
2020-04-14 19:51:30 +0100

    Add clearer error response on create project

--- src/main/java/org/mami/tasktracker/web/ProjectController.java
+++ src/main/java/org/mami/tasktracker/web/ProjectController.java
@@ -6,18 +6,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
 
-    private ProjectService projectService;
+    private final ProjectService projectService;
 
     @Autowired
     public ProjectController(ProjectService projectService) {
@@ -28,7 +31,12 @@ public class ProjectController {
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result) {
 
         if (result.hasErrors()) {
-            return new ResponseEntity<>("invalid project object", HttpStatus.BAD_REQUEST);
+
+             Map<String, String> errors = result.getFieldErrors().stream()
+                     .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+
+
+            return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
         }
 
         Project createdProject = this.projectService.saveOrUpdate(project);
